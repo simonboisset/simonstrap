2c63122305693a61b945e7e2ba9b4dae473dac94
Simon Boisset <sboisset@ambuliz.com>
2021-04-23 00:08:52 +0200

    Update useInput.ts

--- react-mui-kit/src/hooks/useInput.ts
+++ react-mui-kit/src/hooks/useInput.ts
@@ -1,7 +1,7 @@
 import { ValidationError } from 'yup';
 import { UseFormType } from './useForm';
 
-const getInputValue = <T>(name: keyof T, formValue: T, formErrors: ValidationError[] | null) => {
+const getInputValue = <T>(formValue: T, name: keyof T, formErrors: ValidationError[] | null) => {
   const value: T[typeof name] = formValue[name];
   const error =
     formErrors && formErrors.filter(err => err.path === name)[0]
@@ -11,7 +11,7 @@ const getInputValue = <T>(name: keyof T, formValue: T, formErrors: ValidationErr
 };
 
 export const useInput = <T>(form: UseFormType<T>, name: keyof T) => {
-  const { value, error } = getInputValue<T>(name, form.value, form.errors);
+  const { value, error } = getInputValue<T>(form.value, name, form.errors);
   const onChange = (v: T[typeof name]) => form.setFormValue({ ...form.value, [name]: v });
   return { value, error, onChange };
 };
