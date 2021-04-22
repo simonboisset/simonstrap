163e5722b6d651381fa9dfe3222a8ae88dbe134a
Simon Boisset <sboisset@ambuliz.com>
2021-04-23 00:48:30 +0200

    Use Input G of T type value

--- react-mui-kit/src/hooks/useInput.ts
+++ react-mui-kit/src/hooks/useInput.ts
@@ -1,8 +1,8 @@
 import { ValidationError } from 'yup';
 import { UseFormType } from './useForm';
 
-const getInputValue = <T>(formValue: T, name: keyof T, formErrors: ValidationError[] | null) => {
-  const value: T[typeof name] = formValue[name];
+const getInputValue = <T, G extends keyof T>(formValue: T, name: G, formErrors: ValidationError[] | null) => {
+  const value: T[G] = formValue[name];
   const error =
     formErrors && formErrors.filter(err => err.path === name)[0]
       ? formErrors.filter(err => err.path === name)[0].message
@@ -10,8 +10,8 @@ const getInputValue = <T>(formValue: T, name: keyof T, formErrors: ValidationErr
   return { value, error };
 };
 
-export const useInput = <T>(form: UseFormType<T>, name: keyof T) => {
-  const { value, error } = getInputValue<T>(form.value, name, form.errors);
-  const onChange = (v: T[typeof name]) => form.setFormValue({ ...form.value, [name]: v });
+export const useInput = <T, G extends keyof T>(form: UseFormType<T>, name: G) => {
+  const { value, error } = getInputValue<T, G>(form.value, name, form.errors);
+  const onChange = (v: T[G]) => form.setFormValue({ ...form.value, [name]: v });
   return { value, error, onChange };
 };
